e08057971a72204af9550f2dea8fb72c126239d1
Tasin Farhan <tasinfarhan1016@gmail.com>
2025-10-01 17:32:47 +0600

    Completed AI branch

--- src/ttac.c
+++ src/ttac.c
@@ -1,149 +1,122 @@
 #include "ttac.h"
 
-static TTacCell ai_triangle_end(void *game, TTacCell move) {
+static TTacCell ai_draw_end(void *game, TTacCell move) {
   TTacGame *game_ptr = game;
+  TTacCell c1 = game_ptr->c1;
+  TTacCell c2 = game_ptr->c2;
+
+  if (move == c1) {
+    game_ptr->state = TTAC_GAME_DRAW;
+    return c2;
+  }
+
   game_ptr->state = TTAC_GAME_AI_WIN;
-
-  return move == TTAC_CENTER ? TTAC_MIDDLE(game_ptr->control1, game_ptr->control2) : TTAC_CENTER;
-}
-
-static TTacCell ai_triangle(void *game, TTacCell move) {
-  TTacGame *game_ptr = game;
-  TTacCell control1 = game_ptr->control1;
-  TTacCell control2 = game_ptr->control2;
-
-  TTacCell middle = TTAC_MIDDLE(control1, control2);
-
-  if (move != middle) {
-    game_ptr->state = TTAC_GAME_AI_WIN;
-    return middle;
-  }
-
-  game_ptr->branch = ai_triangle_end;
-  game_ptr->control2 = TTAC_OPP_SAME(control2);
-  return game_ptr->control2;
-}
-static TTacCell ai_draw_edge(void *game, TTacCell move) {}
-
-static TTacCell ai_draw_corner(void *game, TTacCell move) {
-  TTacGame *game_ptr = game;
-  TTacCell win = TTAC_LINE(game_ptr->control1, game_ptr->control2);
-
-  if (move != win) {
-    game_ptr->state = TTAC_GAME_AI_WIN;
-    return win;
-  }
-
-  game_ptr->state = TTAC_GAME_DRAW;
-  return TTAC_OPP_SAME(game_ptr->control2);
+  return c1;
 }
 
 static TTacCell ai_draw(void *game, TTacCell move) {
   TTacGame *game_ptr = game;
-  TTacCell control2 = game_ptr->control2;
-  if (TTAC_IS_EDGE(move)) { return TTAC_OPP_SAME(move); }
+  TTacCell c1 = game_ptr->c1;
+  TTacCell c2 = game_ptr->c2;
+  game_ptr->branch = ai_draw_end;
 
-  game_ptr->control2 = TTAC_IS_OPP_DIFF(TTAC_ADJ1_SAME(control2), move) ? TTAC_ADJ1_SAME(control2) : TTAC_ADJ2_SAME(control2);
-  game_ptr->branch = ai_draw_corner;
-  return game_ptr->control2;
+  if (TTAC_IS_EDGE(move)) {
+    TTacCell result = TTAC_OPP_SAME(move);
+    game_ptr->c1 = TTAC_EDGE_CORNER(result, c2);
+    game_ptr->c2 = TTAC_EDGE_CORNER(move, c2);
+    return result;
+  }
+
+  TTacCell result = TTAC_IS_OPP_DIFF(TTAC_ADJ1_SAME(c2), move) ? TTAC_ADJ1_SAME(c2) : TTAC_ADJ1_SAME(c1);
+  game_ptr->c1 = TTAC_EDGE_CORNER(result, c2);
+  game_ptr->c2 = TTAC_OPP_SAME(result);
+  return result;
 }
 
 static TTacCell ai_center(void *game, TTacCell move) {
   TTacGame *game_ptr = game;
-  TTacCell control1 = game_ptr->control1;
-  TTacCell control2 = game_ptr->control2;
+  TTacCell c1 = game_ptr->c1;
+  TTacCell c2 = game_ptr->c2;
 
-  TTacCell middle = TTAC_MIDDLE(control1, control2);
+  TTacCell middle = TTAC_MIDDLE(c1, c2);
   if (move != middle) {
     game_ptr->state = TTAC_GAME_AI_WIN;
     return middle;
   }
 
   game_ptr->branch = ai_draw;
-  game_ptr->control2 = TTAC_OPP_SAME(move);
-  return game_ptr->control2;
+  game_ptr->c1 = move;
+  game_ptr->c2 = TTAC_OPP_SAME(move);
+  return game_ptr->c2;
 }
 
-// static TTacCell ai_corner(void *game, TTacCell move) {{{{
-//   TTacGame *game_ptr = game;
-//   TTacCell control = game_ptr->control1;
-//
-// if (move == TTAC_CENTER) {
-// .............
-// }
-//
-//   // CORNER
-//   if (TTAC_IS_CORNER(move)) {
-//     // ADJACENT
-//     if (TTAC_IS_ADJ_SAME(control, move)) {
-//       TTacCell output = TTAC_OPP_SAME(move);
-//       game_ptr->branch = ai_triangle;
-//       game_ptr->control1 = output;
-//       game_ptr->control2 = control;
-//       return output;
-//     }
-//
-//     // OPPOSITE
-//     game_ptr->branch = ai_triangle;
-//     game_ptr->control2 = TTAC_ADJ1_SAME(move);
-//     return game_ptr->control2;
-//   }
-//
-//   // EDGE
-//   // ADJACENT
-//   if (TTAC_IS_ADJ_DIFF(control, move)) {
-//     game_ptr->control1 = TTAC_LINE_OPP(control, move);
-//     game_ptr->control2 = control;
-//     game_ptr->branch = ai_triangle;
-//     return game_ptr->control1;
-//   }
-//   // OPPOSITE
-//   game_ptr->control2 = (TTAC_IS_ADJ_SAME(TTAC_ADJ1_SAME(control), move) ?: TTAC_ADJ1_SAME(control), TTAC_ADJ2_SAME(control));
-//   game_ptr->branch = ai_triangle;
-//   return game_ptr->control2;
-// }}}}
+static TTacCell triangle_end(void *game, TTacCell move) {
+  TTacGame *game_ptr = game;
+  game_ptr->state = TTAC_GAME_AI_WIN;
+
+  return move == TTAC_CENTER ? TTAC_MIDDLE(game_ptr->c1, game_ptr->c2) : TTAC_CENTER;
+}
+
+static TTacCell triangle(void *game, TTacCell move) {
+  TTacGame *game_ptr = game;
+  TTacCell c1 = game_ptr->c1;
+  TTacCell c2 = game_ptr->c2;
+
+  TTacCell middle = TTAC_MIDDLE(c1, c2);
+
+  if (move != middle) {
+    game_ptr->state = TTAC_GAME_AI_WIN;
+    return middle;
+  }
+
+  game_ptr->branch = triangle_end;
+  game_ptr->c2 = TTAC_OPP_SAME(c2);
+  return game_ptr->c2;
+}
 
 static TTacCell ai_corner(void *game, TTacCell move) {
   TTacGame *game_ptr = game;
-  TTacCell control = game_ptr->control1;
-  TTacCell output;
-  game_ptr->branch = ai_triangle;
+  TTacCell control = game_ptr->c1;
 
+  // Center move
   if (move == TTAC_CENTER) {
     game_ptr->branch = ai_center;
-    game_ptr->control1 = TTAC_ADJ1_SAME(control);
-    game_ptr->control2 = control;
-    return game_ptr->control1;
+    game_ptr->c1 = TTAC_ADJ1_SAME(control);
+    game_ptr->c2 = control;
+    return game_ptr->c1;
   }
 
-  if (TTAC_IS_CORNER(move)) {
-    // corner logic: adjacent vs opposite
-    output = TTAC_IS_ADJ_SAME(control, move) ? TTAC_OPP_SAME(move) : TTAC_ADJ1_SAME(move);
-    TTacBool swap = TTAC_IS_ADJ_SAME(control, move);
+  game_ptr->branch = triangle;
 
-    game_ptr->control1 = swap ? output : control;
-    game_ptr->control2 = swap ? control : output;
-    return output;
-  }
+  const TTacBool is_corner = TTAC_IS_CORNER(move);
 
-  // edge logic: adjacent vs opposite
-  if (TTAC_IS_ADJ_DIFF(control, move)) {
-    output = TTAC_LINE_OPP(control, move);
-    game_ptr->control1 = output;
-    game_ptr->control2 = control;
-    return output;
-  }
+  // clang-format off
+  // Determine adjacency based on type
+  const TTacBool adjacent = is_corner
+                            ? TTAC_IS_ADJ_SAME(control, move)
+                            : TTAC_IS_ADJ_DIFF(control, move);
 
-  // edge opposite
-  output = TTAC_IS_ADJ_SAME(TTAC_ADJ1_SAME(control), move) ? TTAC_ADJ1_SAME(control) : TTAC_ADJ2_SAME(control);
-  game_ptr->control2 = output;
+  // Compute output based on type and adjacency
+  TTacCell output = is_corner
+               ? (adjacent ? TTAC_OPP_SAME(move)
+                           : TTAC_ADJ1_SAME(move))
+               : (adjacent ? TTAC_LINE_OPP(control, move)
+                           : TTAC_ADJ_LINE(control, move));
+  // clang-format on
+
+  // Set control cells
+  game_ptr->c1 = adjacent ? output : control;
+  game_ptr->c2 = adjacent ? control : output;
+
   return output;
 }
 
 static TTacCell ai(void *game, TTacCell move) {
   TTacGame *game_ptr = game;
+
   game_ptr->branch = ai_corner;
-  game_ptr->control1 = TTAC_TOP_LEFT;
+  game_ptr->c1 = TTAC_TOP_LEFT;
+
   return TTAC_TOP_LEFT;
 }
 
@@ -152,6 +125,7 @@ static TTacCell player(void *game, TTacCell move) {
 
   // CORNER
   if (TTAC_IS_CORNER(move)) { return TTAC_CENTER; }
+  // TODO:
 }
 
 void ttac_create_game(TTacGame *game, TTacBool ai_start) {
